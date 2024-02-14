51bfa5c504f1f415b2a58e353fc1f3c07d269674
LxHTT <lxhtt@mcsl.com.cn>
2024-02-14 23:46:09 +0800

    `feat` : logger

---
+++ src/utils/logger.py
@@ -0,0 +1,15 @@
+from loguru import logger
+
+SyncLogger = logger
+
+SyncLogger.add(
+    sink="logs/latest.log",
+    format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}",
+    level="DEBUG",
+    backtrace=True,
+    diagnose=True,
+    rotation="5 MB",
+    compression="zip",
+    retention="10 days",
+)
+SyncLogger.info("MCSL Sync Core is Loading...")
