dec3385287efd03eeb349e1f10759e0dbe2b44c6
LxHTT <lxhtt@mcsl.com.cn>
2024-02-17 21:50:26 +0800

    `feat` : Lightfall

--- src/completion_handler/arclight_powered/base.py
+++ src/completion_handler/arclight_powered/base.py
@@ -17,9 +17,36 @@ class ArclightReleaseSerializer(GitHubReleaseSerializer):
             release.pop("tag_name")
 
         arclight_res = await self.sort_by_mc_versions()
-        with open("data/ArclightPowered/Arclight.json", "wb+") as f:
+        with open("data/Arclight.json", "wb+") as f:
             f.write(dumps(arclight_res, option=OPT_INDENT_2))
         SyncLogger.success("ArclightPowered | Arclight | All versions were loaded.")
+
+    async def sort_by_mc_versions(self) -> list:
+        data_frame = DataFrame(self.release_list)
+        groups = data_frame.groupby("mc_version").groups
+        res = []
+        for version, indices in groups.items():
+            res.append({version: data_frame.loc[indices].to_dict("records")})
+        return res
+
+
+class LightfallReleaseSerializer(GitHubReleaseSerializer):
+    def __init__(self) -> None:
+        super().__init__(user="ArclightPowered", repo="lightfall")
+
+    async def get_assets(self) -> None:
+        await self.get_release_data()
+        for release in self.release_list:
+            release["core_type"] = "Lightfall"
+            release["mc_version"], release["core_version"] = tuple(
+                release["tag_name"].split("-")
+            )
+            release.pop("tag_name")
+
+        lightfall_res = await self.sort_by_mc_versions()
+        with open("data/Lightfall.json", "wb+") as f:
+            f.write(dumps(lightfall_res, option=OPT_INDENT_2))
+        SyncLogger.success("ArclightPowered | Lightfall | All versions were loaded.")
 
     async def sort_by_mc_versions(self) -> list:
         data_frame = DataFrame(self.release_list)
