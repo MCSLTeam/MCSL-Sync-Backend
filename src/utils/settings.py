707f907c2259cca7db14c00c7ab4fc330edcc068
LxHTT <lxhtt@mcsl.com.cn>
2024-04-30 21:09:21 +0800

    feat: secret key

--- src/utils/settings.py
+++ src/utils/settings.py
@@ -1,13 +1,23 @@
 from orjson import loads, dumps, OPT_INDENT_2
-from os import path as osp, makedirs
+from os import path as osp, makedirs, name as osname, getenv, getlogin
 from .logger import SyncLogger
 from .database import init_pre_database, init_production_database
+from platform import processor, system as sysType
+from hashlib import md5
 
 config_template = {
     "url": "0.0.0.0",
     "port": 4523,
     "ssl_cert_path": "",
     "ssl_key_path": "",
+    "secret_key": "".join(
+        [
+            md5(
+                f"{getlogin() if osname == 'nt' else getenv('USER')}{processor()}{sysType()}".encode()
+            ).hexdigest()[i : i + 4]
+            for i in range(0, 24, 1)
+        ]
+    ),
 }
 makedirs("data", exist_ok=True)
 makedirs("logs", exist_ok=True)
