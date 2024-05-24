d70d6a91b574199e2ba99814330e380a8469fcee
Tigercrl <tigercrl@icloud.com>
2024-05-24 20:46:09 +0800

    Revert "fix: CORS"
    This reverts commit 112dd0277c48870d939e87356e7dc47109511260.

--- src/api/model.py
+++ src/api/model.py
@@ -1,10 +1,6 @@
-from quart import Quart, make_response
-from quart_cors import cors, route_cors
+from quart import make_response
 from typing import Union
 from orjson import dumps
-
-app = Quart(__name__)
-app = cors(app, allow_origin="*")  # 允许所有源
 
 
 async def gen_response(
@@ -23,6 +19,5 @@ async def gen_response(
         ),
         status_code,
     )
-    response = route_cors(response, allow_origin="*")  # 允许所有源
     response.mimetype = "application/json"
     return response
